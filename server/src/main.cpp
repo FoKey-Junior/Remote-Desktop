7c608e38c451581ab6b116da69700e001c5387d9
FoKey Junior <149730039+FoKey-Junior@users.noreply.github.com>
2026-03-13 22:54:37 +0300

    update server/main.cpp

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,6 +1,15 @@
 #include "../include/api/Router.hpp"
+#include <iostream>
 
 int main() {
-    Router router;
-    router.start_server(8080);
+    try {
+        Router router;
+        router.start_server(8080);
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
