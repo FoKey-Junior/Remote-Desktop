5339a07f3306a3fc52a99048b2405f8d8b430957
root <ndk52512@gmail.com>
2026-03-16 22:05:41 +0300

    add reading jwt token in routing

--- server/src/api/Router.cpp
+++ server/src/api/Router.cpp
@@ -43,22 +43,40 @@ void Router::start_server(int port_server_) {
 
     CROW_ROUTE(app, "/api/new_command").methods("POST"_method)([database](const crow::request& req) {
         auto body = crow::json::load(req.body);
-        if (!body || !body.has("id") || !body.has("command") || body["id"].t() != crow::json::type::Number || body["command"].t() != crow::json::type::String)
+
+        if (!body ||
+            !body.has("id") || body["id"].t() != crow::json::type::Number ||
+            !body.has("command") || body["command"].t() != crow::json::type::String ||
+            !body.has("token") || body["token"].t() != crow::json::type::String)
             return crow::response(400);
+
+        std::cout << body["token"].s() << std::endl;
         return crow::response(database->add_command(body["id"].i(), body["command"].s()) ? 201 : 500);
     });
 
     CROW_ROUTE(app, "/api/delete_command").methods("POST"_method)([database](const crow::request& req) {
         auto body = crow::json::load(req.body);
-        if (!body || !body.has("id") || body["id"].t() != crow::json::type::Number) return crow::response(400);
+
+        if (!body ||
+            !body.has("id") || body["id"].t() != crow::json::type::Number ||
+            !body.has("command") || body["command"].t() != crow::json::type::String ||
+            !body.has("token") || body["token"].t() != crow::json::type::String)
+            return crow::response(400);
+
+        std::cout << body["token"].s() << std::endl;
         return crow::response(database->delete_command(body["id"].i()) ? 200 : 404);
     });
 
     CROW_ROUTE(app, "/api/get_command").methods("POST"_method)([database](const crow::request& req) {
         auto body = crow::json::load(req.body);
-        if (!body || !body.has("id") || body["id"].t() != crow::json::type::Number) return crow::response(400);
-        auto result = database->get_command(body["id"].i());
+        if (!body ||
+            !body.has("id") || body["id"].t() != crow::json::type::Number ||
+            !body.has("command") || body["command"].t() != crow::json::type::String ||
+            !body.has("token") || body["token"].t() != crow::json::type::String)
+            return crow::response(400);
 
+        std::cout << body["token"].s() << std::endl;
+        auto result = database->get_command(body["id"].i());
         return result.empty() ? crow::response(404) : crow::response(200, result);
     });
 
