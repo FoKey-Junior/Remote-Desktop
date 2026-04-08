6dd067b9392c783660053710fd91c4a8144d7e95
FoKey Junior <149730039+FoKey-Junior@users.noreply.github.com>
2026-04-08 19:53:19 +0300

    fix router

--- server/src/api/Router.cpp
+++ server/src/api/Router.cpp
@@ -31,6 +31,7 @@ void Router::start_server(int port_server_) {
     CROW_ROUTE(app, "/api/registration").methods("POST"_method)([](const crow::request& req) {
         auto data = parse_user(req);
         if (!data) return crow::response(400);
+
         try {
             Registration registration(*data);
             return crow::response(200, registration.get_response());
@@ -43,6 +44,7 @@ void Router::start_server(int port_server_) {
     CROW_ROUTE(app, "/api/authorization").methods("POST"_method)([](const crow::request& req) {
         auto data = parse_user(req);
         if (!data) return crow::response(400);
+
         try {
             Authorization authorization(*data);
             return crow::response(200, authorization.get_response());
@@ -61,7 +63,6 @@ void Router::start_server(int port_server_) {
             !body.has("token") || body["token"].t() != crow::json::type::String)
             return crow::response(400);
 
-        std::cout << body["token"].s() << std::endl;
         return crow::response(database.add_command(body["id"].i(), body["command"].s()) ? 201 : 500);
     });
 
@@ -74,19 +75,18 @@ void Router::start_server(int port_server_) {
             !body.has("token") || body["token"].t() != crow::json::type::String)
             return crow::response(400);
 
-        std::cout << body["token"].s() << std::endl;
         return crow::response(database.delete_command(body["id"].i()) ? 200 : 404);
     });
 
     CROW_ROUTE(app, "/api/get_command").methods("POST"_method)([&database](const crow::request& req) {
         auto body = crow::json::load(req.body);
+
         if (!body ||
             !body.has("id") || body["id"].t() != crow::json::type::Number ||
             !body.has("command") || body["command"].t() != crow::json::type::String ||
             !body.has("token") || body["token"].t() != crow::json::type::String)
             return crow::response(400);
 
-        std::cout << body["token"].s() << std::endl;
         auto result = database.get_command(body["id"].i());
         return result.empty() ? crow::response(404) : crow::response(200, result);
     });
